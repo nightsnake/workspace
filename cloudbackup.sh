4a4a884fb0b2221f39d7060ca239624a19d6ad23
snake <auchakin@gmail.com>
2017-05-29 23:11:08 +0300

    partial update

--- cloudbackup.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/expect
-spawn mount.davfs https://webdav.yandex.ru /storage/st02/cloud/Yandex.Disk
-expect "Username:"
-send "snakeu\r"
-expect "Password:"
-send "K13T;R&Oa$.sZmCXtiJ@\r"
-expect eof
